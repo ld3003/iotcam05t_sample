6ae00a49f1ecd1efcfaf20b335de0031e30403ee
liudan <liudan@easy-iot.cc>
2021-01-28 00:08:57 +0800

    add paho mqtt

---
+++ paho.mqtt.c/build.sh
@@ -0,0 +1,5 @@
+mkdir build
+cd build
+cmake .. -DPAHO_BUILD_STATIC=TRUE -DCMAKE_C_COMPILER=/media/user1/QC2101A/software/prebuilt/gcc/linux-x86/arm/toolchain-sean-musl/toolchain/bin/arm-openwrt-linux-muslgnueabi-gcc
+
+
